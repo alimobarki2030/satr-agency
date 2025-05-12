abbf5b98268cf34b01f8de1341550d496b4cfdbc
alimobarki2030 <alimobarki.ad@gmail.com>
2025-05-12 12:33:30 +0300

    🧼 تنظيف الكود وإزالة تكرار dir=rtl ومحاذاة يدوية

---
+++ pages/document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="ar" dir="rtl">
+      <Head />
+      <body className="bg-[#030712] text-white">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
