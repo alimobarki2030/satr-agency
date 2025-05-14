a9889d31491bdaad08f2f935a58dbcb3b1984e91
alimobarki2030 <alimobarki.ad@gmail.com>
2025-05-14 12:37:39 +0300

    إضافة Google Analytics في _document.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { motion, useAnimation } from "framer-motion";
 import { useEffect } from "react";
 import { FaBullhorn, FaSearch, FaStore, FaPenNib, FaPalette, FaChartLine } from "react-icons/fa";
-import Head from "next/head";
 
 
 const services = [
@@ -48,19 +47,6 @@ export default function Home() {
     
     <>
 
-       <Head>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="سطر | وكالة تسويق رقمي تصنع حضورك بنتائج حقيقية" />
-        <meta name="twitter:description" content="وكالة تسويق رقمي سعودية تقدم خدمات المحتوى، التصميم، الإعلانات، والسيو." />
-        <meta name="twitter:image" content="https://www.satr.agency/og-banner.png" />
-        <meta name="twitter:url" content="https://www.satr.agency/" />
-        <meta name="twitter:site" content="@satragency_" />
-        <meta name="twitter:creator" content="@satragency_" />
-        <meta property="og:title" content="سطر | وكالة تسويق رقمي تصنع حضورك بنتائج حقيقية" />
-        <meta property="og:description" content="سطر وكالة سعودية متخصصة في المحتوى والإعلانات وتحسين الظهور، نقودك بنتائج ملموسة." />
-        <meta property="og:image" content="https://www.satr.agency/og-banner.png" />
-        <meta property="og:url" content="https://www.satr.agency/" />
-      </Head>
       <main className="min-h-screen bg-[#030712] text-white flex flex-col items-center justify-center px-6 relative">
         <img src="/images/logo.png" alt="شعار وكالة سطر" className="w-32 md:w-40 mb-6" />
 
@@ -295,7 +281,7 @@ export default function Home() {
   </div>
 </section>
 
-     <footer className="bg-[#0f172a] text-white px-6 pt-12 pb-6">
+     <footer className="bg-[#020407] text-white px-6 pt-12 pb-6">
   <div className="max-w-6xl mx-auto flex flex-col items-center text-center gap-6">
 
     {/* الشعار */}
@@ -320,6 +306,3 @@ export default function Home() {
   );
 }
 
-export async function getServerSideProps() {
-  return { props: {} };
-}
