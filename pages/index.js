e0a679f7b1bd4c6e864cae7bc97194aa68a82796
alimobarki2030 <alimobarki.ad@gmail.com>
2025-05-14 12:25:22 +0300

    تعديل سكشن تواصل + تحسين تنسيق

--- pages/index.js
+++ pages/index.js
@@ -295,11 +295,6 @@ export default function Home() {
   </div>
 </section>
 
-
-
-
-
-
      <footer className="bg-[#0f172a] text-white px-6 pt-12 pb-6">
   <div className="max-w-6xl mx-auto flex flex-col items-center text-center gap-6">
 
@@ -321,10 +316,6 @@ export default function Home() {
     </div>
   </div>
 </footer>
-
-
-
-
     </>
   );
 }
