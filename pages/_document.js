e87c34eab91d19af00f4c6e43734d6ffeebb64a5
alimobarki2030 <alimobarki.ad@gmail.com>
2025-05-14 12:54:09 +0300

    Fix: remove broken @vercel/analytics import

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
 
 class MyDocument extends Document {
   render() {
