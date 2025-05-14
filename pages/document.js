5a0549e48859ec900c26ad1c699fa47dcffab3cd
alimobarki2030 <alimobarki.ad@gmail.com>
2025-05-14 12:31:06 +0300

    إضافة Google Analytics في _document.js

--- pages/document.js
+++ pages/document.js
@@ -1,6 +1,25 @@
 // pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+<Head>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="سطر هي وكالة تسويق رقمي سعودية..." />
+  <meta name="keywords" content="..." />
+
+  {/* Google tag (gtag.js) */}
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-08S7E28LJG"></script>
+  <script dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-08S7E28LJG');
+    `
+  }} />
+</Head>
+
+
 class MyDocument extends Document {
   render() {
     return (
